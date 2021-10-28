91e6aacf1467d9ab763c300312309074e5c4df7e
Knyazev Igor <xaskell@yandex.ru>
2021-10-28 11:05:46 +0300

    expire time added

--- cache-go.go
+++ cache-go.go
@@ -1,24 +1,32 @@
 package cacheGo
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
+
+const defaultTTL = 5 * time.Second
 
 type Cache struct {
-	pair map[string]interface{}
-	mu   *sync.RWMutex
+	pair       map[string]interface{}
+	mu         *sync.RWMutex
+	expireTime time.Time
 }
 
 func New() Cache {
 	return Cache{
-		pair: make(map[string]interface{}),
-		mu:   new(sync.RWMutex),
+		pair:       make(map[string]interface{}),
+		mu:         new(sync.RWMutex),
+		expireTime: time.Now().Add(defaultTTL),
 	}
 }
 
 // set only if cache is not null (memory allocated)
-func (c *Cache) Set(key string, value interface{}) {
+func (c *Cache) Set(key string, value interface{}, ttl time.Duration) {
 	if c != nil {
 		c.mu.Lock()
 		c.pair[key] = value
+		c.expireTime = time.Now().Add(ttl)
 		c.mu.Unlock()
 	}
 }
